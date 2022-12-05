e47e2714ee60d4781bf8c7698c0633fb981788cb
anhmdao <118239857+anhmdao@users.noreply.github.com>
2022-12-05 16:47:25 +0700

    Update index.js

--- src/components/Contain/index.js
+++ src/components/Contain/index.js
@@ -6,41 +6,34 @@ const Contain = () => {
     return (
         <div className={cx('Contain')}>
             <div className={cx('text')}>
-                <p>Burger</p>
+                <div className={cx('text-p1')}>
+                    Burger
+                </div>
+                <div className={cx('text-logo')}>
+                    <img alt='img1' src='/BurgerSlash.png' />
+                </div>
+                <div className={cx('text-p2')}>
+                    <p>
+                        Our NEW all-natural wings are hormone and steroid free, sourced from local USA farms. Every order is freshly made, crisped to perfection, hand tossed, and served with a side of ranch. Available in Garlic Rosemary, Scorchin’ Hot, or Savory BBQ.
+                    </p>
+                </div>
+                <div className={cx('box-text')}>
+                    <div className={cx('text-p3')}>
+                        <p> $15.25 </p>
+                    </div>
+                    <div className={cx('text-p4')}>
+                        <p>$20.25</p>
+                        <img alt='vector' src='/Vector3.png' />
+                    </div>
+                </div>
+                <div className={cx('text-button')}>
+                    <a href="#" >Order Now</a>
+                </div>
             </div>
             <div className={cx('img')}>
-                <img alt='img' src='/BurgerSlash.png' />
+                <img alt='img' src='/img.png' />
             </div>
-            <div className={cx('text1')}>
-                <p>Our NEW all-natural wings are hormone and steroid free, sourced from local USA farms. Every order is freshly made, crisped to perfection, hand tossed, and served with a side of ranch. Available in Garlic Rosemary, Scorchin’ Hot, or Savory BBQ</p>
-            </div>
-            <div className={cx('img1')}>
-                <img alt='img1' src='/Artboard51.png' />
-            </div>
-            <div className={cx('img2')}>
-                <img alt='img2' src='Artboard81.png' />
-            </div>
-            <div className={cx('img3')}>
-                <img alt='img3' src='/Burger-Img.png' />
-            </div>
-            <div className={cx('img4')}>
-                <img alt='img4' src='/Artboard71.png' />
-            </div>
-            <div className={cx('img5')}>
-                <img alt='img5' src='/Artboard101.png' />
-            </div>
-            <div className={cx('text2')}>
-                <p>$15.25</p>
-            </div>
-            <div className={cx('vector')}>
-                <img alt='vector' src='/Vector3.png' />
-            </div>
-            <div className={cx('text3')}>
-                <p>$20.25</p>
-            </div>
-            <div className={cx('button')}>
-            <a href="#" className= {cx('btn')}>Order Now</a>
-            </div>
+
         </div>
     )
 }
